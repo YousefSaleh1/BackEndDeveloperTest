cce3a39f71397cc7db3cb2fb1fa9ae36789ddb69
YousefSaleh1 <148710592+YousefSaleh1@users.noreply.github.com>
2024-10-16 22:44:59 +0300

    implement JWT authentication and secure task routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService;
+use app\Traits\ApiResponseTrait;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // Trait used to standardize API responses
+    use ApiResponseTrait;
+
+    protected $AuthService;
+
+    /**
+     * AuthController constructor.
+     *
+     * Initializes the AuthService instance which handles business logic related to authentication.
+     *
+     * @param AuthService $AuthService The service responsible for handling authentication.
+     */
+    public function __construct(AuthService $AuthService)
+    {
+        $this->AuthService = $AuthService;
+    }
+
+    /**
+     * Registers a new user.
+     *
+     * @param RegisterRequest $request The request containing user registration data.
+     * @return \Illuminate\Http\JsonResponse Returns a success response with the registered user.
+     */
+    public function register(RegisterRequest $request)
+    {
+        $user = $this->AuthService->register($request->validated());
+
+        return $this->successResponse($user, 'User registered successfully', 201);
+    }
+
+    /**
+     * Logs in a user.
+     *
+     * @param LoginRequest $request The request containing login credentials.
+     * @return \Illuminate\Http\JsonResponse Returns a success response with user and token data.
+     */
+    public function login(LoginRequest $request)
+    {
+        $loginResult = $this->AuthService->login($request->validated());
+
+        return $this->apiResponse($loginResult['user'], $loginResult['token'], 'Login successful', 200);
+    }
+
+    /**
+     * Logs out the authenticated user.
+     *
+     * @param Request $request The request containing the authenticated user.
+     * @return \Illuminate\Http\JsonResponse Returns a success response after logging out.
+     */
+    public function logout(Request $request)
+    {
+        $this->AuthService->logout($request->user());
+
+        return $this->successResponse(null, 'Logged out successfully');
+    }
+}
